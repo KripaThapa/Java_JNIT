7f511da8f8fff48dbca6d6e536a0073ca26354a7
Kripa <creationinmind@gmail.com>
2020-04-30 17:53:43 -0500

    while assignment

---
+++ While.java
@@ -0,0 +1,15 @@
+class WhileDemo{
+          public static void main(String args[]){
+	int i = 1; 
+ 	int n = 1;
+	
+	while(n<=50){
+	        n = 5 * i;
+	        System.out.println("5 x " + i + " = " + n);
+	        n++;
+	        i++;
+	}
+          }
+
+
+}

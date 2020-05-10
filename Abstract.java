5bf184debfd247cbe324d7d55b981e6003430f77
Kripa <creationinmind@gmail.com>
2020-05-09 21:06:24 -0500

    added Abstract

--- Abstract.java
+++ Abstract.java
@@ -1,15 +1,11 @@
 abstract class Animal{
 abstract void lifespan();
-abstract void breath();
+void breath(){System.out.println("They breath air");};
 }
 
 class Tiger extends Animal{
           void lifespan(){
 	System.out.println("Average lifespan of tiger is 10-15yrs");
-          }
-
-          void breath(){
-	System.out.println("Tiger breath air through their nose");
           }
 }
 
@@ -17,11 +13,6 @@ class Mudskipper extends Animal{
          void lifespan(){
 	System.out.println("Average lifespan of Mudskipper is 5yrs");
           }
-
-          void breath(){
-	System.out.println("Mudskippers breath air through their nose as well as they can get oxygen through their skin");
-          }
-
 }
 
 class AbstractDemo{
